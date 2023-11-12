d3d0355463eb1eaa4c273a9098706deb1d28f36e
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-11-12 16:46:32 -0500

    added feed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,19 +4,36 @@ export default function Sidebar() {
   return(
     <div className="sidebar"> 
       <div className="sidebarWrapper"> 
-          
-           <ul> Events </ul>
-           <ul> Jobs </ul>
-           <ul> Employers </ul>
-           <ul> Chats </ul>
-           <ul> Career center </ul>
-           <ul> Courses </ul>
-           <ul> Chats </ul>
-           <ul> Bookmarks </ul>
-           <ul> Questions </ul>
-          
-      </div>
-    </div>
-     
+        <ul className="sidebarList"> 
+            <li className="sidebarList"> 
+              <span className ="sidebarListItemText">Events </span>
+            </li>
+            <li className="sidebarList"> 
+              <span className ="sidebarListItemText">Jobs </span>
+            </li>
+            <li className="sidebarList"> 
+              <span className ="sidebarListItemText">Employers </span>
+            </li>
+            <li className="sidebarList"> 
+              <span className ="sidebarListItemText">Chats </span>
+            </li>
+            <li className="sidebarList"> 
+              <span className ="sidebarListItemText">Career center </span>
+              </li>
+            <li className="sidebarList"> 
+              <span className ="sidebarListItemText">Courses </span>
+            </li>
+            <li className="sidebarList"> 
+              <span className ="sidebarListItemText">Career center </span>
+            </li>
+            <li className="sidebarList"> 
+              <span className ="sidebarListItemText">Bookmarks </span>
+            </li>
+            <li className="sidebarList"> 
+             <span className ="sidebarListItemText">Questions </span>
+            </li>
+          </ul>
+        </div>
+      </div>      
   )
 }
