1ea8f259e3192e9b09b740e942ef3b31eefd5ce2
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-11-11 19:23:58 -0500

    Added side bar and Feed

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,12 @@
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+
+
 export default function Home() {
-  return <div>
-    This is real home page
-  </div>
+  return (
+    <>
+      <Topbar />
+      <Sidebar />
+    </>
+  );
 }
