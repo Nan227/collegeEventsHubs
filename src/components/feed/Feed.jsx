ac3a7b31c63b6fd174f3d62cb43c00b325bbd88b
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-11-14 15:38:37 -0500

    added card

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -7,8 +7,8 @@ export default function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
-      <div class="card" style="width: 18rem;">
-  <img src="/assets/companies/google.jpg" class="card-img-top" alt=""/>
+      <div class="card">
+  <img src="/assets/companies/google.jpg" alt=""/>
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
