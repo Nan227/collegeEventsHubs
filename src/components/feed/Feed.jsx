e82f6028472a653ac9f6f45fddb540f6c56a2d09
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-11-14 15:33:58 -0500

    delete Hihi ansd Share components

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,13 +1,20 @@
 
-import React from 'react';
+
 import "./feed.css" ;
-import Share from './Share'; // Adjust the path accordingly
+
 
 export default function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
-        <Share />
+      <div class="card" style="width: 18rem;">
+  <img src="/assets/companies/google.jpg" class="card-img-top" alt=""/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
       </div>
     </div>
   );
