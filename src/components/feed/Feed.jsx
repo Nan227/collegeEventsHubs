8f14d4b4b3c62005a808e3413da5933968f06723
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-11-14 21:12:35 -0500

    edit the topbar high

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,20 +1,51 @@
-
-
-import "./feed.css" ;
-
+import React from 'react';
+import './feed.css';
 
 export default function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
-      <div class="card">
-  <img src="/assets/companies/google.jpg" alt=""/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+        <div class="card">
+          <img src="/assets/companies/google.jpg" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="/assets/companies/facebook.png" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="/assets/companies/ig.jpg" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
