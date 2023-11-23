83a17a827262406e53f7b67401463fbf4de19fe4
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-11-22 20:39:29 -0500

    Added loging page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import "./login.css";
+
+export default function login() {
+  return (
+    <div className = "login">
+       <div className = "loginWrapper">
+          <div className = "loginLeft">
+          <h3 className = "loginLogo"> College Events Hubs </h3>
+          <span className=" loginDesc">Welcome to Events Hubs </span>
+            
+          </div>
+          <div className = "loginRight">
+          <div className = "loginBox"></div>
+          </div>
+       </div>
+    </div>
+  )
+}
