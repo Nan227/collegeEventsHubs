9512544456514fda771246bfc13faec39df53ff0
Piyaporn Puangprasert <dngooddeals@gmail.com>
2023-11-22 21:42:47 -0500

    Added login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from './pages/home/Home';
-import Profile from './pages/profile/Profile'
-import Login from './pages/login/'
+import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
 
 function App() {
   return (
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,12 +5,17 @@ export default function login() {
     <div className = "login">
        <div className = "loginWrapper">
           <div className = "loginLeft">
-          <h3 className = "loginLogo"> College Events Hubs </h3>
-          <span className=" loginDesc">Welcome to Events Hubs </span>
+            <h3 className = "loginLogo"> College Events Hubs </h3>
+            <span className=" loginDesc">Welcome to Events Hubs </span>
             
           </div>
           <div className = "loginRight">
           <div className = "loginBox"></div>
+            <input placeholder="Email" className="loginInput"/>
+            <input placeholder="Password" className="loginInput"/>
+            <button className ="loginButton">Log In</button>
+            <span className ="loginForgot">Forgot Password</span>
+            <button className ="loginRegisterButton">Create a New Account</button>
           </div>
        </div>
     </div>
